d4ee0375428bd9a326d1cf28ddbe57aaac1efae0
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2020-11-17 01:07:25 +0100

    main func almost ended

--- dbconnector.py
+++ dbconnector.py
@@ -3,19 +3,21 @@ import psycopg2
 
 
 def config(filename='database.ini', section='postgresql'):
+    try:
+        parser = ConfigParser()
+        parser.read('database.ini')
 
-    parser = ConfigParser()
-    parser.read('database.ini')
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
+        return db
+    except FileNotFoundError as error:
+        print(error)
 
 
 def connect_to_db():
@@ -25,21 +27,10 @@ def connect_to_db():
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
         return conn
-        # cur = conn.cursor()
-        # cur.execute('SELECT version()')
-        #
-        # db_version = cur.fetchone()
-        # print(db_version)
-        #
-        # # close the communication with the PostgreSQL
-        # cur.close()
+
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
-    #         print('Database connection closed.')
 
-
-if __name__ == '__main__':
-    connect_to_db()
+#
+# if __name__ == '__main__':
+#     connect_to_db()
--- main.py
+++ main.py
@@ -1,30 +1,19 @@
+from datetime import date
+
 import requests
 
+from dbmanipulation import *
 from pingpong import start
 
 
 def get_json_from_api():
-    #get location of counters
-    url = 'https://data.mobility.brussels/bike/api/counts/?request=devices'
-    request = requests.get(url)
-    json = request.json()
-    count = 0
-    for j in json['features']:
-        print(len(json['features']))
-        print(json['features'][count]['properties']['road_en'])
-        print(json['features'][count]['id'])
-        count+=1
-
-    #test of one device
-    url_live = 'https://data.mobility.brussels/bike/api/counts/?request=live&featureID=CAT17'
-    live_req= requests.get(url_live)
-    json_live = live_req.json()
-    print('live data ',json_live)
+    pass
 
 
 
 
 if __name__ == '__main__':
+
     get_json_from_api()
     start()
 
